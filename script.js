7806b22b232f3fc6c59894b65afd33437cbd4315
chimdevit <delevid25@gmail.com>
2025-05-29 22:08:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPanel() {
+  document.getElementById("myPanel").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closePanel() {
+  document.getElementById("myPanel").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
